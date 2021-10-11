2df9901b2ab1da76daa261853136f908fb5a5b42
Bart Van Bos <bartvanbos@gmail.com>
2021-10-11 19:45:24 +0200

    enable sudo systemctl start systemd-resolved.service

--- udf/init/update.sh
+++ udf/init/update.sh
@@ -5,3 +5,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 sudo apt-get -y auto-remove
+
+echo "Enable systemd-resolved service"
+sudo systemctl start systemd-resolved.service
+sudo systemctl enable systemd-resolved.service
